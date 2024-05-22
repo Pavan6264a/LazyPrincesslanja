c029f509e36a36ab3359622d8cde35ac24664fc6
Pavan6264a <127077968+Pavan6264a@users.noreply.github.com>
2024-05-22 20:33:06 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,17 +2,10 @@ if [ -z "$UPSTREAM_REPO" ]; then
   echo "Cloning main Repository"
   git clone https://github.com/Pavan6264a/LazyPrincesslanja.git /LazyPrincess
 else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO"
-  git clone "$UPSTREAM_REPO" /LazyPrincess
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /LazyPrincess
 fi
-
 cd /LazyPrincess
-
-if [ -f "requirements.txt" ]; then
-  pip3 install -U -r requirements.txt
-else
-  echo "requirements.txt not found. Skipping installation of dependencies."
-fi
-
+pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
